cd6448f4a0e7d02ad701a9c1829f0be65f543539
Marcos Brito <marcos.brito0507@gmail.com>
2025-09-23 15:05:49 -0300

    feat: add lua implementation to run some plugins

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod lua;
+
+pub fn say_hi(name: &str) {
+    println!("Hi, {name}");
+}
---
+++ src/lua.rs
@@ -0,0 +1,39 @@
+use anyhow::Result;
+use mlua::{Lua, Result as LuaResult};
+use std::{fs, path::Path};
+
+pub struct Runtime {
+    vm: Lua,
+}
+
+impl Runtime {
+    pub fn new() -> Self {
+        Runtime { vm: Lua::new() }
+    }
+
+    pub fn init(&self) -> Result<()> {
+        let say_hi = self.vm.create_function(say_hi)?;
+
+        self.vm.globals().set("say_hi", say_hi)?;
+
+        Ok(())
+    }
+
+    pub fn exec(&self, source: &str) -> Result<()> {
+        self.vm.load(source).exec()?;
+
+        Ok(())
+    }
+
+    pub fn exec_file<P: AsRef<Path>>(&self, path: P) -> Result<()> {
+        let source = fs::read_to_string(path)?;
+
+        self.exec(&source)
+    }
+}
+
+
+fn say_hi(_: &Lua, name: String) -> LuaResult<()> {
+    super::say_hi(&name);
+    Ok(())
+}
